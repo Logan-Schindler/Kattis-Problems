f8c16a404dcc6bc2352f262210a0a91d7dc573f7
Logan-Schindler <105387772+Logan-Schindler@users.noreply.github.com>
2022-09-29 11:15:03 -0500

    Add files via upload

---
+++ 3dprinter.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <bits/stdc++.h>
+#include<algorithm>
+using namespace std;
+
+int main()
+{
+	int statues;
+	cin >> statues;
+	int days = 1;
+	
+	while (statues > 1){
+	    if(statues % 2 == 0){
+	        statues = statues / 2;
+	    }
+	    else{
+	        statues = statues / 2;
+	        statues +=1;
+	    }
+	    days++;
+	}
+	cout << days << endl;
+	return 0;
+}
